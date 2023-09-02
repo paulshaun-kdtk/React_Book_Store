c01ef590a2bebe68d82b0d61bf453148b40113f1
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-09-02 12:06:18 +0200

    Application setup

---
+++ src/components/Components.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.css';
+import Header from './Header';
+
+function Components() {
+  return (
+    <div className="Container">
+      <Header />
+      <div>
+        Components
+      </div>
+
+    </div>
+  );
+}
+
+export default Components;
